651a45ec72ca65c78b1297f4e98e50d8f6061fa9
Nahum Maurice <nahum.maurice.nm@gmail.com>
2023-03-12 04:34:03 +0300

    Bugfix: fixed empty messages crashing

--- server.go
+++ server.go
@@ -103,36 +103,39 @@ func (s *Server) readLoop(conn net.Conn) {
 		// Conduct a test to see whether the msg is a command. If it
 		// is a command, it should be handled as such. The way it's
 		// handled is defined by the commands packages.
-		if msg[0] == '/' {
-			command := NewCommand(msg)
-			command.Handle(conn)
-		} else {
-			curr_user, err := GetUserByConnectionAddress(conn.RemoteAddr().String())
-			// If the user is not authenticated, we should not allow them to send
-			// messages to any room. This could be changed later, but for now, that's
-			// the best possible behaviour I think for the sake of simplicity.
-			if err != nil {
-				conn.Write([]byte("\n[System] ::: You are not authenticated. Please log in by typing '/auth <username> <password>'.\n\n"))
+		if len(msg) != 0 {
+			if msg[0] == '/' {
+				command := NewCommand(msg)
+				command.Handle(conn)
 			} else {
-				// If the user didn't join any room, we should neither allow them to send
-				// messages to anywhere. This could change later but for the sake of
-				// simplicity, let's keep it like that now
-				destination_room, err := curr_user.GetCurrentRoom()
+				curr_user, err := GetUserByConnectionAddress(conn.RemoteAddr().String())
+				// If the user is not authenticated, we should not allow them to send
+				// messages to any room. This could be changed later, but for now, that's
+				// the best possible behaviour I think for the sake of simplicity.
 				if err != nil {
-					msg := fmt.Sprintf(
-						"\n" +
-							"[System] ::: Please, join a room. To see all rooms, type '/rooms'.\n" +
-							"........     To create a new room, type '/newroom <name>'.\n" +
-							"........     To join an existing room, type '/join <room>'.\n\n")
-					conn.Write([]byte(msg))
+					conn.Write([]byte("\n[System] ::: You are not authenticated. Please log in by typing '/auth <username> <password>'.\n\n"))
 				} else {
-					// When a message is sent, we then need to send the message to the
-					// room where the sender is currently present and therefore broadcast
-					// the message to the other members of the room.
-					formated_message := NewMessage(curr_user, msg, destination_room)
-					s.MessageChannel <- formated_message
+					// If the user didn't join any room, we should neither allow them to send
+					// messages to anywhere. This could change later but for the sake of
+					// simplicity, let's keep it like that now
+					destination_room, err := curr_user.GetCurrentRoom()
+					if err != nil {
+						msg := fmt.Sprintf(
+							"\n" +
+								"[System] ::: Please, join a room. To see all rooms, type '/rooms'.\n" +
+								"........     To create a new room, type '/newroom <name>'.\n" +
+								"........     To join an existing room, type '/join <room>'.\n\n")
+						conn.Write([]byte(msg))
+					} else {
+						// When a message is sent, we then need to send the message to the
+						// room where the sender is currently present and therefore broadcast
+						// the message to the other members of the room.
+						formated_message := NewMessage(curr_user, msg, destination_room)
+						s.MessageChannel <- formated_message
+					}
 				}
 			}
 		}
+
 	}
 }
